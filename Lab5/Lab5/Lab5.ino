482e58ca6e51728256b5eeef0780e1cb9ee0bfde
pel19072 <68125920+pel19072@users.noreply.github.com>
2021-03-29 23:52:13 -0600

    Update Lab5.ino
    Se combinan las dos partes. Aún no funciona, hay un problema con la lectura del Serial.

--- Lab5/Lab5/Lab5.ino
+++ Lab5/Lab5/Lab5.ino
@@ -1,14 +1,17 @@
 #include <SPI.h>
 #include <SD.h>
-
+File myFile;
 File root;
+String document = "hola";
+unsigned char eleccion = 0x00;
+
+void show(void);
+void choice(void);
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
   SPI.setModule(0);
-  pinMode(RED_LED, OUTPUT);
-  digitalWrite(RED_LED, HIGH);
 
   Serial.print("Initializing SD card...");
   pinMode(PA_3, OUTPUT);
@@ -18,17 +21,19 @@ void setup()
     return;
   }
   Serial.println("initialization done.");
-
-  root = SD.open("/");
-
-  printDirectory(root, 0);
-
-  Serial.println("done!");
+  show();
 }
 
 void loop()
 {
-  // nothing happens after setup finishes.
+  eleccion = Serial.read();
+  Serial.println(eleccion);
+  //choice();
+}
+
+void show(void) {
+  root = SD.open("/");
+  printDirectory(root, 0);
 }
 
 void printDirectory(File dir, int numTabs) {
@@ -52,5 +57,41 @@ void printDirectory(File dir, int numTabs) {
       Serial.println(entry.size(), DEC);
     }
     entry.close();
+  }
+}
+
+void choice(void) {
+  switch (eleccion) {
+    case 0:
+      break;
+    case 1:
+      document = "yoshi.txt";
+      Serial.println(document);
+      eleccion = 0;
+      break;
+    case 2:
+      document = "yoshi.txt";
+      Serial.println(document);
+      eleccion = 0;
+      break;
+    case 3:
+      document = "yoshi.txt";
+      Serial.println(document);
+      eleccion = 0;
+      break;
+  }
+  myFile = SD.open(document);
+  if (myFile) {
+    Serial.println(document);
+
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening document");
   }
 }
