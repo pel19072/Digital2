94b0cd497d2a9853e352c2ebb3b0883f469bfc98
pel19072 <68125920+pel19072@users.noreply.github.com>
2021-05-21 15:22:37 -0600

    Finalizado
    Se agregaron pdfs y comentarios.

--- Proyecto4/Main/Main.ino
+++ Proyecto4/Main/Main.ino
@@ -36,7 +36,7 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Try Connecting to ");
   Serial.println(ssid);
-
+  // Pines para display
   pinMode(5, OUTPUT);
   pinMode(12, OUTPUT);
   pinMode(14, OUTPUT);
@@ -44,7 +44,7 @@ void setup() {
   pinMode(19, OUTPUT);
   pinMode(26, OUTPUT);
   pinMode(27, OUTPUT);
-
+  //Inicialización de los pines
   digitalWrite(5, HIGH);
   digitalWrite(12, HIGH);
   digitalWrite(14, HIGH);
@@ -68,7 +68,7 @@ void setup() {
   server.on("/", handle_OnConnect); // Directamente desde e.g. 192.168.0.8
 
   server.onNotFound(handle_NotFound);
-
+  // Creación del servidor local
   server.begin();
   Serial.println("HTTP server started");
   delay(100);
@@ -79,15 +79,19 @@ void setup() {
 void loop() {
   server.handleClient();
   if (Serial2.available() > 0) {
+    // Lectura serial, lee los 5 datos y almacena los 4 que no son un enter
     Serial2.readBytesUntil(10, sensor, 4);
     delay(20);
   }
   temporal = 0;
   for (int i = 0; i < 4; i++) {
+    // Cuento qué parqueos están disponibles
     if (sensor[i] == 0x31) {
       temporal++;
     }
   }
+  // Se llama a la tabla de conversión del número de parqueos 
+  // disponibles para que se muestre en el display
   number();
 }
 //************************************************************************************************
@@ -95,6 +99,7 @@ void loop() {
 //************************************************************************************************
 void handle_OnConnect() {
   Serial.println("GPIO2 Status: OFF");
+  // Crea el código de html
   server.send(200, "text/html", SendHTML());
 }
 
@@ -102,6 +107,7 @@ void handle_OnConnect() {
 // Procesador de HTML
 //************************************************************************************************
 String SendHTML() {
+  // Creación del código HTML -Tomado del documento html Main en el directorio data-
   String ptr = "<!--Comentario-->\n";
   ptr += "<!doctype>\n";
   ptr += "\n";
@@ -110,75 +116,54 @@ String SendHTML() {
   ptr += "\t\t<title>Proyecto 4</title>  <!--Este título se guarda en la pestaña, no en la página como tal-->\n";
   ptr += "\t\t<meta charset=\"utf-8\"/>\n";
   ptr += "\t\t<style>\n";
-  ptr += "*{text-align:center;}\n";
-  ptr += "\n";
-  ptr += "h1, h2{color:#ffffff;}\n";
-  ptr += "\n";
-  ptr += "div.uno{\n";
-  ptr += "\tcolor:black;\n";
-  ptr += "\talign: center;\n";
-  ptr += "\tpadding: 28px;\n";
-  ptr += "\tbackground-color: #f7523b;\n";
-  ptr += "\tfont-family: \"Montserrat\", sans-serif;\n";
-  ptr += "\theight:20%;\n";
-  ptr += "}\n";
-  ptr += "\n";
-  ptr += "div.dos {\n";
-  ptr += "\tfloat: center;\n";
-  ptr += "\tpadding: 28px;\n";
-  ptr += "\tbackground-color:#FFFFFF;\n";
-  ptr += "}\n";
-  ptr += "\n";
-  ptr += "div.tres {\n";
-  ptr += "\tpadding: 28px;\n";
-  ptr += "\tfont-family: courier;\n";
-  ptr += "\tfloat: center;\n";
-  ptr += "\theight:3%;\n";
-  ptr += "\tbackground-color: #f77b6a;\n";
-  ptr += "\tborder-width: 0.05xp;\n";
-  ptr += "}\n";
-  ptr += "\n";
-  ptr += "img{\n";
-  ptr += "\tdisplay:block;\n";
-  ptr += "\tmargin:auto;\n";
-  ptr += "}\n";
-  ptr += "\n";
-  ptr += "img.extra{\n";
-  ptr += "\twidth: 100px;\n";
-  ptr += "}\n";
-  ptr += "\n";
-  ptr += "p.texto{\n";
-  ptr += "\ttext-indent:2cm;\n";
-  ptr += "\ttext-align:justify;\n";
-  ptr += "}\n";
-  ptr += "\n";
-  ptr += "table {\n";
-  ptr += "  width: 75%;\n";
-  ptr += "\tfloat: center;\n";
-  ptr += "}\n";
-  ptr += "\n";
-  ptr += "th, td {\n";
-  ptr += "  padding: 15px;\n";
-  ptr += "  text-align: center;\n";
-  ptr += "\tborder-bottom: 1px solid #ddd;\n";
-  ptr += "}\n";
-  ptr += "\n";
-  ptr += "tr:nth-child(even) {background-color: #f2f2f2;}\n";
-  ptr += "tr:hover {background-color: #f8f8f8;}\n";
-  ptr += "\n";
-  ptr += "th {\n";
-  ptr += "  background-color: #f7523b;\n";
-  ptr += "  color: white;\n";
-  ptr += "}\n";
-  ptr += "\n";
-  ptr += "td {\n";
-  ptr += "  height: 50px;\n";
-  ptr += "  vertical-align: center;\n";
-  ptr += "}\n";
-  ptr += "";
-  ptr += "\t\t</style>\n";
+  ptr += "\t\t\th1, h2{color:#ffffff;}\n";
+  ptr += "\t\t\tdiv.uno{\n";
+  ptr += "\t\t\t\ttext-align: center;\n";
+  ptr += "\t\t\t\tcolor:black;\n";
+  ptr += "\t\t\t\talign: center;\n";
+  ptr += "\t\t\t\tpadding: 14px;\n";
+  ptr += "\t\t\t\tbackground-color: #f7523b;\n";
+  ptr += "\t\t\t\tfont-family: \"Montserrat\", sans-serif;\n";
+  ptr += "\t\t\t\theight:20%;\n";
+  ptr += "\t\t\t\twidth: 100%;\n";
+  ptr += "\t\t\t}\n";
+  ptr += "\t\t\tdiv.dos {\n";
+  ptr += "\t\t\t\tfloat: left;\n";
+  ptr += "\t\t\t\twidth: 100%;\n";
+  ptr += "\t\t\t\theight: auto;\n";
+  ptr += "\t\t\t\tpadding: 8px;\n";
+  ptr += "\t\t\t\tbackground-color:#ffffff;\n";
+  ptr += "\t\t\t}\n";
+  ptr += "\t\t\tdiv.tres {\n";
+  ptr += "\t\t\t\ttext-align: center;\n";
+  ptr += "\t\t\t\tbackground-color: #f77b6a;\n";
+  ptr += "\t\t\t\tfont-family:courier;\n";
+  ptr += "\t\t\t\tfloat: center;\n";
+  ptr += "\t\t\t\twidth: 100%;\n";
+  ptr += "\t\t\t\theight: auto;\n";
+  ptr += "\t\t\t}\n";
+  ptr += "\t\t\ttable {\n";
+  ptr += "\t\t\t\twidth: 100%;\n";
+  ptr += "\t\t\t\tfloat: center;\n";
+  ptr += "\t\t\t}\n";
+  ptr += "\t\t\tth, td {\n";
+  ptr += "\t\t\t  padding: 15px;\n";
+  ptr += "\t\t\t  text-align: center;\n";
+  ptr += "\t\t\t\tborder-bottom: 1px solid #ddd;\n";
+  ptr += "\t\t\t}\n";
+  ptr += "\t\t\ttr:nth-child(even) {background-color: #f2f2f2;}\n";
+  ptr += "\t\t\ttr:hover {background-color: #f8f8f8;}\n";
+  ptr += "\t\t\tth {\n";
+  ptr += "\t\t\t  background-color: #f7523b;\n";
+  ptr += "\t\t\t  color: white;\n";
+  ptr += "\t\t\t}\n";
+  ptr += "\t\t\ttd {\n";
+  ptr += "\t\t\t  height: 50px;\n";
+  ptr += "\t\t\t  vertical-align: center;\n";
+  ptr += "\t\t\t}\n";
+  ptr += "\t\t</style>";
   ptr += "\t</head>\n";
-  ptr += "\t\n";
+  ptr += "\n";
   ptr += "\t<body>\n";
   ptr += "\n";
   ptr += "\t<div class=\"uno\">\n";
@@ -188,62 +173,64 @@ String SendHTML() {
   ptr += "\t</header> <!--1-->\n";
   ptr += "\t</div>\n";
   ptr += "\n";
+  ptr += "\n";
+  ptr += "\t<br>\n";
+  ptr += "\t<br>\n";
+  ptr += "\n";
   ptr += "\t<div class=\"dos\">\n";
-  ptr += "\t<section> <!--3-->\n";
+  ptr += "\t<section> <!--2-->\n";
   ptr += "    <table>\n";
   ptr += "      <tr>\n";
   ptr += "        <th>Número de parqueo</th>\n";
   ptr += "        <th>Disponibilidad</th>\n";
   ptr += "      </tr>\n";
   ptr += "      <tr>\n";
-  ptr += "        <td>Parqueo 1</td>\n";
-  if (sensor[0] == 0x31) {
-    ptr += "        <td>Disponible &#x2705;</td>\n";
+  ptr += "        <td>Parqueo 1 &#x1F693;</td>\n";
+  if (sensor[0] == 0x31){
+    ptr += "        <td>Disponible &#x2705;</td>";
   }
-  else {
-    ptr += "        <td>Ocupado &#9940;</td>\n";
-    temporal2 = 1;
-  }
-  ptr += "      </tr>\n";
-  ptr += "      <tr>\n";
-  ptr += "        <td>Parqueo 2</td>\n";
-  if (sensor[1] == 0x31) {
-    ptr += "        <td>Disponible &#x2705;</td>\n";
-  }
-  else {
-    ptr += "        <td>Ocupado &#9940;</td>\n";
-    temporal2 = 2;
+  else{
+    ptr += "        <td>Ocupado &#9940;</td>";
   }
   ptr += "      </tr>\n";
   ptr += "      <tr>\n";
-  ptr += "        <td>Parqueo 3</td>\n";
-  if (sensor[2] == 0x31) {
-    ptr += "        <td>Disponible &#x2705;</td>\n";
+  ptr += "        <td>Parqueo 2 &#x1F695;</td>\n";
+  if (sensor[1] == 0x31){
+    ptr += "        <td>Disponible &#x2705;</td>";
   }
-  else {
-    ptr += "        <td>Ocupado &#9940;</td>\n";
-    temporal2 = 3;
+  else{
+    ptr += "        <td>Ocupado &#9940;</td>";
   }
   ptr += "      </tr>\n";
   ptr += "      <tr>\n";
-  ptr += "        <td>Parqueo 4</td>\n";
-  if (sensor[3] == 0x31) {
-    ptr += "        <td>Disponible &#x2705;</td>\n";
+  ptr += "        <td>Parqueo 3 &#x1F697;</td>\n";
+  if (sensor[2] == 0x31){
+    ptr += "        <td>Disponible &#x2705;</td>";
   }
-  else {
-    ptr += "        <td>Ocupado &#9940;</td>\n";
-    temporal2 = 4;
+  else{
+    ptr += "        <td>Ocupado &#9940;</td>";
+  }
+  ptr += "      </tr>\n";
+  ptr += "      <tr>\n";
+  ptr += "        <td>Parqueo 4 &#x1F699;</td>\n";
+  if (sensor[3] == 0x31){
+    ptr += "        <td>Disponible &#x2705;</td>";
+  }
+  else{
+    ptr += "        <td>Ocupado &#9940;</td>";
   }
   ptr += "      </tr>\n";
   ptr += "    </table>\n";
   ptr += "\t</section>\n";
   ptr += "\t</div>\n";
-  ptr += "\n";
   ptr += "\t<div class=\"tres\">\n";
-  ptr += "\t<footer>\n";
-  ptr += "\t\tProyecto por Ricardo Pellecer <br>\n";
-  ptr += "\t\tElectrónica Digital 2\n";
-  ptr += "\t</footer> <!--5-->\n";
+  ptr += "\t<p>\n";
+  ptr += "\t\t\tParqueos Disponibles:";
+  ptr +=  String(temporal); 
+  ptr += "<br>\n";
+  ptr += "\t\t\tProyecto por Ricardo Pellecer <br>\n";
+  ptr += "\t\t\tElectrónica Digital 2\n";
+  ptr += "\t\t</p> <!--3-->\n";
   ptr += "\t</div>\n";
   ptr += "\n";
   ptr += "  <script>\n";
@@ -254,8 +241,7 @@ String SendHTML() {
   ptr += "  </script>\n";
   ptr += "\n";
   ptr += "\t</body>\n";
-  ptr += "</html>\n";
-  ptr += "";
+  ptr += "</html>";
   return ptr;
 }
 //************************************************************************************************
@@ -265,6 +251,7 @@ void handle_NotFound() {
   server.send(404, "text/plain", "Not found");
 }
 
+// Conversión de número para colocar en display
 void tabla(uint8_t a, uint8_t b, uint8_t c, uint8_t d, uint8_t e, uint8_t f, uint8_t g) {
   if (g == 1) {
     digitalWrite(12, HIGH);
@@ -303,6 +290,7 @@ void tabla(uint8_t a, uint8_t b, uint8_t c, uint8_t d, uint8_t e, uint8_t f, uin
   }
 }
 
+// Coloca las 5 opciones posibles.
 void number(void) {
   switch (temporal) {
     case 0:
